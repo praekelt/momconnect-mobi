4c7346412a37bddf6261e6e24ad5e7211e33fa87
Jenna <lkxjen002@myuct.ac.za>
2016-12-07 16:39:15 +0200

    Changed logo to an ImageField in models

--- faq_site/questions/models.py
+++ faq_site/questions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Catagory(models.Model):
     catagory_title = models.CharField(max_length=250)
-    catagory_logo = models.FileField()
+    catagory_logo = models.ImageField()
 
     def __str__(self):
         return self.catagory_title
