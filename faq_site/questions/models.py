0cb8faeaf5a183f1fde042b8164a05bf134655f6
Jenna <lkxjen002@myuct.ac.za>
2016-12-07 13:54:06 +0200

    Added method to create string representation of the QandA object

--- faq_site/questions/models.py
+++ faq_site/questions/models.py
@@ -13,3 +13,6 @@ class QandA(models.Model):
     catagory = models.ForeignKey(Catagory, on_delete=models.CASCADE)
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.question
