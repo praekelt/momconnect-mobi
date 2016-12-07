f376044d4af3d0cc3318e27a5becfed645c8e958
Jenna <lkxjen002@myuct.ac.za>
2016-12-07 13:45:35 +0200

    Added QandA class to the questions app models.py file, inorder to store questions and answers associated with each catagory

--- faq_site/questions/models.py
+++ faq_site/questions/models.py
@@ -7,3 +7,9 @@ class Catagory(models.Model):
 
     def __str__(self):
         return catagory_title
+
+
+class QandA(models.Model):
+    catagory = models.ForeignKey(Catagory, on_delete=models.CASCADE)
+    question = models.CharField(max_length=500)
+    answer = models.CharField(max_length=2000)
