ab0209a7fcd917a4f06a19f2ffad06a9dea32819
Jenna <lkxjen002@myuct.ac.za>
2016-12-07 13:33:57 +0200

    Added catagory class to questions app models.py file, which will allow question catagories to be added and removed by admin, each containing title and logo

--- faq_site/questions/models.py
+++ faq_site/questions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Catagory(models.Model):
+    catagory_title = models.CharField(max_length=250)
+    catagory_logo = models.FileField()
+
+    def __str__(self):
+        return catagory_title
