ba5d812f7cf8f93a5fa325548ed6ce83530e1864
Jenna <lkxjen002@myuct.ac.za>
2016-12-08 09:57:14 +0200

    Created initial views corresponding to urls

--- faq_site/questions/views.py
+++ faq_site/questions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse("This page will list question catagories")
+
+
+def questions(request, catagory_name):
+    return HttpResponse("This page will list questions"
+                        " relating to a particular catagory")
+
+
+def answers(request, catagory_name, question_id):
+    return HttpResponse("This page with display "
+                        "individual question answer pairs")
