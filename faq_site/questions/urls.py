9f6f01d04285e0ecbb9b08ed225d25d65e546ba9
Jenna <lkxjen002@myuct.ac.za>
2016-12-08 12:00:50 +0200

    changed structure of questions app urls to include questions base url

--- faq_site/questions/urls.py
+++ faq_site/questions/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<catagory_name>[a-zA-Z]+)/$', views.questions, name='questions'),
-    url(r'^(?P<catagory_name>[a-zA-Z]+)/(?P<question_id>[0-9]+)/$',
+    url(r'^questions/$', views.index, name='index'),
+    url(r'^questions/(?P<catagory_name>[a-zA-Z]+)/$', views.questions, name='questions'),
+    url(r'^questions/(?P<catagory_name>[a-zA-Z]+)/(?P<question_id>[0-9]+)/$',
         views.answers, name='answers'),
 ]
