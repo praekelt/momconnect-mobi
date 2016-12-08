50efd265f91490ce93a222debcd5459c06cb466e
Jenna <lkxjen002@myuct.ac.za>
2016-12-08 12:03:28 +0200

    added home view to questions app views

--- faq_site/questions/views.py
+++ faq_site/questions/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Catagory, QandA
 
 
 # Create your views here.
+def home(request):
+    return HttpResponse("This is the home page")
+
+
 def index(request):
+    # all_catagories = Catagory.objects.all()
     return HttpResponse("This page will list question catagories")
 
 
 def questions(request, catagory_name):
+    # catagory_questions =
     return HttpResponse("This page will list questions"
                         " relating to a particular catagory")
 
