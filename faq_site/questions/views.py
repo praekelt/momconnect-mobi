67b126b714d710f7734b9cf172fd706e89547fbe
Jenna <lkxjen002@myuct.ac.za>
2016-12-09 10:57:39 +0200

    Corrected flake8 errors

--- faq_site/questions/views.py
+++ faq_site/questions/views.py
@@ -10,17 +10,20 @@ def home(request):
 
 def index(request):
     all_catagories = Catagory.objects.all()
-    return render(request, 'questions/index.html', {'all_catagories': all_catagories})
+    return render(request, 'questions/index.html',
+                  {'all_catagories': all_catagories})
 
 
 def questions(request, catagory_name):
-    catagory=Catagory.objects.get(catagory_title=catagory_name)
-    #return HttpResponse("Displaying" + catagory.catagory_title + "!")
+    catagory = Catagory.objects.get(catagory_title=catagory_name)
     catagory_questions = catagory.qanda_set.all()
-    return render(request, 'questions/questions.html', {'catagory': catagory, 'catagory_questions': catagory_questions})
+    return render(request, 'questions/questions.html',
+                  {'catagory': catagory,
+                   'catagory_questions': catagory_questions})
 
 
 def answers(request, catagory_name, question_id):
-    catagory=Catagory.objects.get(catagory_title=catagory_name)
-    question=QandA.objects.get(id=question_id)
-    return render(request, 'questions/answers.html', {'catagory': catagory, 'question': question})
+    catagory = Catagory.objects.get(catagory_title=catagory_name)
+    question = QandA.objects.get(id=question_id)
+    return render(request, 'questions/answers.html',
+                  {'catagory': catagory, 'question': question})
