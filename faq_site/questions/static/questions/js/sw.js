d9211d56a35acc170a4a0ae9536c39b098b1a4b2
Jenna <lkxjen002@myuct.ac.za>
2016-12-13 11:11:52 +0200

    added service worker javascript file with core functionality

---
+++ faq_site/questions/static/questions/js/sw.js
@@ -0,0 +1,13 @@
+//I removed my service worker code and opted for basic demo code for testing
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
+});
