033f760d040ed3245b04646e77b26aa271603d6a
Jenna <lkxjen002@myuct.ac.za>
2016-12-22 13:40:24 +0200

    Added comments to admin file

--- faq_site/questions/admin.py
+++ faq_site/questions/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Catagory, QandA
 
+# Registered models in admin
 admin.site.register(Catagory)
 admin.site.register(QandA)
-
-# Register your models here.
