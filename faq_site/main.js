f5768bd7b531608e25d286fafcf0ea8c1c16952e
Jenna <lkxjen002@myuct.ac.za>
2016-12-22 13:39:17 +0200

    Added comments to main.js file

--- faq_site/main.js
+++ faq_site/main.js
@@ -1,7 +1,10 @@
+// First checks if service worker is available in browser
+// Then attempts to register service worker, using promises
+
+
 if ('serviceWorker' in navigator) {
 
     console.log('Service Worker is supported');
-
     navigator.serviceWorker.register('/sw.js')
     .then(function(reg) {
         console.log("Service Worker successfully registered", reg);
